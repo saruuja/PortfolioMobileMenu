83a3bdd0bea3b071e281f22f58b8f060434d3c98
saruuja <saaruuja42@gmail.com>
2022-06-14 15:08:42 +0300

    js file

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const sideNav = document.getElementById('sideNav');
 const menu = document.getElementById('menu');
 
 menuBtn.onclick = function () {
-  if (sideNav.style.top === '-250px') {
-    sideNav.style.top = '90px';
+  if (sideNav.style.top === '-600px') {
+    sideNav.style.top = '91px';
     menu.src = 'img/close.png';
   } else {
-    sideNav.style.top = '-250px';
+    sideNav.style.top = '-600px';
     menu.src = 'img/menu.png';
   }
 };
